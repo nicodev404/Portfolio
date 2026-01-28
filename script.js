00f27de0b7c57b6f54c0138bd4fb8d0ca7de6b2d
fabricionicolasok <fabricionicolascarranza@outlook.com>
2026-01-28 03:35:41 -0300

    Code update and cleanup

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-let toggle = document.getElementById('toggle');
-let labelToggle = document.getElementById('labelToggle');
-toggle.addEventListener('change', (event) =>{
-    let checked = event.target.checked;
-    document.body.classList.toggle('light-mode');
-    if(checked){
-        labelToggle.innerHTML = '<i class="fa-solid fa-sun"></i>';
-        labelToggle.style.color = "yellow";
-    }
-    else{
-        labelToggle.innerHTML = '<i class="fa-solid fa-moon"></i>';
-        labelToggle.style.color = "rebeccapurple";
-    }
+ let toggle = document.getElementById('toggle');
+ let labelToggle = document.getElementById('labelToggle');
+ toggle.addEventListener('change', (event) =>{
+     let checked = event.target.checked;
+     document.body.classList.toggle('light-mode');
+     if(checked){
+         labelToggle.innerHTML = '<i class="fa-solid fa-moon"></i>';
+         labelToggle.style.color = "rebeccapurple";
+     }
+     else{
+         labelToggle.innerHTML = '<i class="fa-solid fa-sun"></i>';
+         labelToggle.style.color = "yellow";
+     }
 })
