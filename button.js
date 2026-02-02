f925972ab19d294b323927f30afcc3f75b5c6354
fabricionicolasok <fabricionicolascarranza@outlook.com>
2026-02-02 17:59:40 -0300

    Cleaning the code

--- button.js
+++ button.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     const btnMenu = document.getElementById('btn-menu');
-    const navLinks = document.querySelector('.nav-links'); // Usamos la clase para asegurar
+    const navLinks = document.querySelector('.nav-links');
 
     if (btnMenu) {
         btnMenu.addEventListener('click', () => {
             navLinks.classList.toggle('active');
-            console.log("¡Click detectado!"); // Mira la consola (F12) para confirmar
+            console.log("¡Click detectado!");
         });
     }
 });
